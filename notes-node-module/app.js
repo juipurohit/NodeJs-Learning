07b043cb049ea31c7bc367c305d90dd0c814eec2
INFOSTRETCH\jui.purohit <jui.purohit@infostretch.com>
2018-08-31 12:13:35 +0530

    Changes regarding demo

--- notes-node-module/app.js
+++ notes-node-module/app.js
@@ -1,21 +1,5 @@
 console.log('Starting app.');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
-
-var user = os.userInfo();
-var filteredArray = _.uniq(['Jui',1,2,'Jui',3,4,1,5,'jui']);
-// var res= notes.addNote();
-// console.log(user);
-// console.log(res);
-console.log(filteredArray);
-console.log('Result : ',notes.add(2,-3));
-console.log('For abc :',_.isString('abc'));
-console.log('For 1 :',_.isString(1));
-fs.appendFile('greetings.txt',`Hello ${user.username} ! You are ${notes.age}`, function(err){
-    if(err){
-        console.log('Unable to write file');
-    }
-});
+const notes = require('./notes.js');
--- notes-node-module/notes.js
+++ notes-node-module/notes.js
@@ -1,12 +1,4 @@
-console.log('Inside notes.js');
-
-module.exports.age = 25;
-
 module.exports.addNote = () => {
     console.log('add Node!');
     return 'New Note!'
-};
-
-module.exports.add = (a,b) => {
-    return a + b;
 };
